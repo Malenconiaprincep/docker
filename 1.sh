e2092614e4cc2a869a9ca09bef2898acc774de99
makuta <wangbo@tvapk.net>
2016-07-22 14:43:30 +0800

    update

---
+++ 1.sh
@@ -0,0 +1,6 @@
+echo "# docker" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:Malenconiaprincep/docker.git
+git push -u origin master
---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FORM readytalk/nodejs
+
+WORKDIR /app
+ADD package.json /app/
+RUN npm install
+ADD . /app
+
+CMD []
+ENTRYPOINT ["/nodejs/bin/npm", "start"]
+
